5975abc469e3a71daa38eddc176dc9d263bf2939
“Anais <ananavelez@gmail.com>
2017-07-07 19:30:10 -0400

    modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,11 @@ $(document).ready(function() {
 		console.log("success");
 		console.log(res);
 		for (var i = 1; i < 719; i++) {
-			var pokePic = $("<img src=https://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
-			$('div.pokemon').append(pokePic);
+			var poke = $("<a class='waves-effect waves-light btn' href='#modal1'><div class = 'col l3 m3 s3 xs1'><img class = 'pokepoke' src=http://pokeapi.co/media/img/" + i + ".png id=" + i + "/></div></a>")
+			$('div.pokemon').append(poke);
 		}
+		$('#modal1').modal('open');
+		$('#modal1').modal('close');
 	})
 	.fail(function() {
 		console.log("error");
