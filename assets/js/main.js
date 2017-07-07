824b2886ba4002ca9197b109b696c5ddddadd368
anisen <ananavelez@gmail.com>
2017-07-07 12:40:10 -0400

    ajustando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,25 @@
 $(document).ready(function() {
-
-	for (var i = 1; i < 152; i++) {
-		var pokePic = $("<img src=http://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
-		$('div.pokemon').append(pokePic);
-	}
-
-	$('img', this).click(function() {
-
-		var uniqueId = $(this).attr('id');
-
-		var webAddress = "http://pokeapi.co/api/v1/pokemon/" + uniqueId;
-
-		var types = " ";
-
-		$.get(webAddress, function(res) {
-			for (var j = 0; j < res.types.length; j++) {
-				types += "<li>" + res.types[j].name + "</li>";
-
+	$.ajax({
+		url: 'https://pokeapi.co/api/v2/pokemon/',
+		type: 'GET',
+		dataType: 'JSON',
+		data: {"limit": '811'},
+	})
+	.done(function(res) {
+		console.log("success");
+		console.log(res);
+		for (var i = 1; i < 719; i++) {
+			var pokePic = $("<img src=https://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
+			$('div.pokemon').append(pokePic);
+			for (var x=1 ;x<i.length; x++) {
+				
 			}
-		}, 'json');
-
-		$.get(webAddress, function(res) {
-
-			$('div.pokedex').html(
-				"<h1>" + res.name + "</h1>" +
-				"<img src=http://pokeapi.co/media/img/" + uniqueId + ".png>" +
-				"<h4>Types:</h4>" +
-				"<ul>" +
-				types +
-				"</ul>" +
-				"<h4>Height:</h4>" +
-				"<p>" + res.height + "</p>" +
-				"<h4>Weight:</h4>" +
-				"<p>" + res.weight + "</p>"
-				);
-
-		}, 'json');
-
-	});
+		}
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	})
 });
-$('#myModal').on('shown.bs.modal', function () {
-			$('#myInput').focus()
-		})
